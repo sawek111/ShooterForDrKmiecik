08b2e95914c4afff1d828daa48e91dbcfe3379ab
Saper <sawek111@gmail.com>
2018-06-09 13:51:33 +0200

    <Settings Installer> fixed && player added

--- ShooterForDrKmiecik/Assets/Scripts/Installers/SettingsInstaller.cs
+++ ShooterForDrKmiecik/Assets/Scripts/Installers/SettingsInstaller.cs
@@ -4,13 +4,16 @@ using Zenject;
 [CreateAssetMenu(fileName = "Settings", menuName = "Installers/Settings")]
 public class SettingsInstaller : ScriptableObjectInstaller<SettingsInstaller>
 {
-    private MenuInstaller.Settings _menuSettings = null;
-    private GameplayInstaller.Settings _gameplaySettings = null;
+    [SerializeField] private MenuInstaller.Settings _menuSettings = null;
+    [SerializeField] private GameplayInstaller.Settings _gameplaySettings = null;
 
+    [SerializeField]  private Player.Settings _player = null;
 
     public override void InstallBindings()
     {
         Container.BindInstance(_menuSettings);
         Container.BindInstance(_gameplaySettings);
+
+        Container.BindInstance(_player);
     }
 }
